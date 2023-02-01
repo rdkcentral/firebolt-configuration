0cb732352bc51fab8160ca340fd62c755150b34e
Kevin Pearson <kevin_pearson@cable.comcast.com>
2023-02-01 11:14:39 -0800

    feat: Generate the spec and commit it to source control

--- src/js/spec/index.mjs
+++ src/js/spec/index.mjs
@@ -5,7 +5,10 @@ const run = async () => {
   const RPC = JSON.parse(await readFile('./dist/json/firebolt.json'))
   const majors = Object.keys(RPC.apis)
   for (let i = 0; i < majors.length; i++) {
-    const version = RPC.apis[majors[i]].info.version
+    let version = RPC.apis[majors[i]].info.version
+    if (version.indexOf('-') !== -1) {
+      version = version.substring(0, version.indexOf('-')) // strip pre-release
+    }
     writeFile('specs/firebolt-' + version + '.json', JSON.stringify(RPC, null, 2))
   }
 }
