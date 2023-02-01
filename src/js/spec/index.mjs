2eb137653378420e7a2a87aeffe2025af01f5f64
Kevin Pearson <kevin_pearson@cable.comcast.com>
2023-02-01 11:04:34 -0800

    feat: Generate the spec and commit it to source control

--- src/js/spec/index.mjs
+++ src/js/spec/index.mjs
@@ -1,8 +1,13 @@
 import { promises } from 'fs'
 const { readFile, writeFile } = promises
-const RPC = JSON.parse(readFile('dist/json/firebolt.json'))
-const majors = Object.keys(RPC.apis)
-for (let i = 0; i < majors.length; i++) {
-  const version = RPC.apis[majors[i]].info.version
-  writeFile('specs/firebolt-' + version + '.json', JSON.stringify(RPC))
+
+const run = async () => {
+  const RPC = JSON.parse(await readFile('./dist/json/firebolt.json'))
+  const majors = Object.keys(RPC.apis)
+  for (let i = 0; i < majors.length; i++) {
+    const version = RPC.apis[majors[i]].info.version
+    writeFile('specs/firebolt-' + version + '.json', JSON.stringify(RPC, null, 2))
+  }
 }
+
+run()
